82854f17b07c1bad158ab2e83990363fedf4e2c8
Josh Stancombe <61159870+josh-stancombe@users.noreply.github.com>
2021-04-02 10:20:22 +0100

    Add files via upload

---
+++ 1. 8 Ball/code.py
@@ -0,0 +1,34 @@
+import random
+
+name = "Josh"
+question = "Am i alive?"
+answer = ""
+randomNumber = random.randint(1,9)
+
+if randomNumber == 1:
+  answer = "Yes - definitely"
+elif randomNumber == 2:
+  answer = "It is decidedly so"
+elif randomNumber == 3:
+  answer = "answer3"
+elif randomNumber == 4:
+  answer = "answer4"
+elif randomNumber == 5:
+  answer = "answer5"
+elif randomNumber == 6:
+  answer = "answer6"
+elif randomNumber == 7:
+  answer = "answer7"
+elif randomNumber == 8:
+  answer = "answer8"
+elif randomNumber == 9:
+  answer = "answer9"
+else:
+  answer = "Answer Error"
+
+print(name + " asks: " + question)
+print("Magic balls answer: " + answer)
+
+# Try this in a Python emulator instead os static variables above?
+# name = input("What is your name? ")
+# question = input("What is your question? ")
